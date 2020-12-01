54169b42b4dec735eb856a5b6dbed16e0360f0ff
Sadamori Kojaku <skojaku@iu.edu>
2020-11-30 22:38:54 -0500

    update bug

--- ent2id/Ent2Id.py
+++ ent2id/Ent2Id.py
@@ -1,6 +1,12 @@
 import numpy as np
 from scipy import sparse
+import numba
+from numba import types
+from numba.typed import Dict
+from collections import Counter
 
+#
+import pandas as pd
 import nltk
 from nltk.util import ngrams
 from nltk.corpus import stopwords
@@ -8,6 +14,8 @@ from nltk.corpus import stopwords
 from tqdm import tqdm
 import re
 from collections.abc import Iterable
+
+from joblib import Parallel, delayed
 
 
 class Ent2Id:
@@ -24,12 +32,14 @@ class Ent2Id:
 
         self.subword_mode = "word"
         self.init_stop_words(languages)
+        self.n_jobs = 10
 
         self.clear()
 
     def clear(self):
-        self.ent_list = []
         self.subword2id = {}
+        self.ent2id = {}
+        self.id2ent = {}
         self.link_list = []
         self.W = None
 
@@ -83,22 +93,25 @@ class Ent2Id:
 
     def partial_fit(self, ent_name_list, id_list):
 
+        # Add to dictionary
+        W = None
         # Construct vocabrary
-        self.W = None
-        self.ent_list += list(id_list)
         for i, ent_name in enumerate(tqdm(ent_name_list, desc="Generating vocabulary")):
-            words = self.generate_ngrams_range(ent_name, self.ns, self.nf)
+            words = generate_ngrams_range(
+                ent_name, self.ns, self.nf, self.subword_mode, self.stopwords
+            )
+            self.ent2id[id_list[i]] = self.ent2id.get(id_list[i], len(self.ent2id))
+            j = self.ent2id[id_list[i]]
             for word in words:
                 self.subword2id[word] = self.subword2id.get(word, len(self.subword2id))
-                self.link_list += [(self.subword2id[word], id_list[i])]
+                self.link_list += [(self.subword2id[word], j)]
         return self
 
     def make_ent2id_mapping(self):
 
-        uid_list = np.unique(self.ent_list)
-        self.id2ent = dict(zip(np.arange(len(self.ent_list)), self.ent_list))
+        self.id2ent = {value: key for key, value in self.ent2id.items()}
         self.nword = len(self.subword2id)
-        self.nent = len(self.ent_list)
+        self.nent = len(self.id2ent)
 
         # Make character ngram vs entity matrix
         link_list = np.array(self.link_list)
@@ -106,8 +119,7 @@ class Ent2Id:
             (np.ones(link_list.shape[0]), (link_list[:, 0], link_list[:, 1])),
             shape=(self.nword, self.nent),
         )
-        self.B = self.W.copy()
-        self.B.data = np.ones_like(self.B.data)
+        self.link_list = []  # clear list
 
         self.dr = np.array(self.W.sum(axis=1)).reshape(-1)
         self.dc = np.array(self.W.sum(axis=0)).reshape(-1)
@@ -116,15 +128,51 @@ class Ent2Id:
         def safe_log(v):
             return np.log(np.maximum(v, 1e-12))
 
-        self.minlog = np.log(1e-12)
         r, c, v = sparse.find(self.W)
         v = safe_log(v) - (
             safe_log(self.dr[r]) + safe_log(self.dc[c]) - safe_log(self.denom)
         )
-        v += -self.minlog
+        v += 1e-3  # offset log(v) is actually zero but it will be treated as nothing. Therefore add a small value as an offset
         self.W = sparse.csr_matrix((v, (r, c)), shape=(self.nword, self.nent),)
 
     def predict(self, input_text_list):
+        if self.W is None:
+            self.make_ent2id_mapping()
+
+        if isinstance(input_text_list, str):
+            input_text_list = [input_text_list]
+
+        word_ids_list = []
+        query_ids = []
+        for qid, input_text in enumerate(input_text_list):
+            word_ids = np.array(
+                [
+                    self.subword2id.get(s, np.nan)
+                    for s in generate_ngrams_range(
+                        input_text, self.ns, self.nf, self.subword_mode, self.stopwords
+                    )
+                ]
+            )
+            word_ids = np.unique(word_ids[~np.isnan(word_ids)])
+            word_ids_list += [word_ids.astype(int)]
+            query_ids += [qid * np.ones(len(word_ids))]
+
+        # Calculate Score
+        query_ids = np.concatenate(query_ids).astype(int)
+        word_ids = np.concatenate(word_ids_list).astype(int)
+        V = sparse.csr_matrix(
+            (np.ones_like(word_ids), (query_ids, word_ids)),
+            shape=(len(word_ids_list), self.nword),
+        )
+        score = V @ self.W
+
+        entids, score = row_argmax(
+            score.data, score.indices, score.indptr, score.shape[0], score.shape[1]
+        )
+        entids = [self.id2ent[i] for i in entids]
+        return entids, score
+
+    def predict_test(self, input_text_list):
         if self.W is None:
             self.make_ent2id_mapping()
 
@@ -136,61 +184,57 @@ class Ent2Id:
             word_ids = np.array(
                 [
                     self.subword2id.get(s, np.nan)
-                    for s in self.generate_ngrams_range(input_text, self.ns, self.nf)
+                    for s in generate_ngrams_range(
+                        input_text, self.ns, self.nf, self.subword_mode, self.stopwords
+                    )
                 ]
             )
             word_ids = word_ids[~np.isnan(word_ids)]
-            word_ids_list += [word_ids]
+            word_ids_list += [word_ids.astype(int)]
 
-        score = self.calc_score(word_ids_list)
-        hits = np.array(score.argmax(axis=1)).reshape(-1)
-        return (
-            [self.id2ent[h] for h in hits],
-            np.array([score[i, h] for i, h in enumerate(hits)]),
-        )
+        return self.find_best_match(word_ids_list)
 
-    def calc_score(self, word_ids_list):
-        query_ids = np.concatenate(
-            [np.ones(len(word_ids)) * k for k, word_ids in enumerate(word_ids_list)]
-        ).astype(int)
-        word_ids = np.concatenate(word_ids_list).astype(int)
 
-        V = sparse.csr_matrix(
-            (np.ones_like(word_ids), (query_ids, word_ids)),
-            shape=(len(word_ids_list), self.nword),
-        )
-        V.data = np.ones_like(V.data)
+# Function to generate n-grams from sentences.
+def generate_word_ngrams(s, num, stopwords):
+    # Convert to lowercases
+    s = s.lower()
+    s = re.sub(r"[^a-zA-Z0-9\s]", " ", s)
+    s = re.sub(r" +", " ", s)
+    tokens = [
+        token for token in s.split(" ") if (token != "") and (not token in stopwords)
+    ]
+    return [" ".join(token) for token in list(ngrams(tokens, num))]
 
-        score = V @ self.W
-        return score
 
-    # Function to generate n-grams from sentences.
-    def generate_word_ngrams(self, s, num):
-        # Convert to lowercases
-        s = s.lower()
-        s = re.sub(r"[^a-zA-Z0-9\s]", " ", s)
-        s = re.sub(r" +", " ", s)
-        tokens = [
-            token
-            for token in s.split(" ")
-            if (token != "") and (not token in self.stopwords)
-        ]
-        return [" ".join(token) for token in list(ngrams(tokens, num))]
+def generate_ngrams(s, n):
+    # Convert to lowercases
+    s = s.lower()
+    # Replace all none alphanumeric characters with spaces
+    s = re.sub(r"[^a-zA-Z0-9\s]", " ", s)
+    s = re.sub(r" +", " ", s)
+    return ["".join(n) for n in ngrams(s, n)]
 
-    def generate_ngrams(self, s, n):
-        # Convert to lowercases
-        s = s.lower()
 
-        # Replace all none alphanumeric characters with spaces
-        s = re.sub(r"[^a-zA-Z0-9\s]", " ", s)
-        s = re.sub(r" +", " ", s)
-        return ["".join(n) for n in ngrams(s, n)]
+def generate_ngrams_range(s, ns, nf, subword_mode, stopwords):
+    retval = []
+    for n in np.arange(ns, nf + 1):
+        if subword_mode == "word":
+            retval += generate_word_ngrams(s, n, stopwords)
+        elif subword_mode == "char":
+            retval += generate_ngrams(s, n)
+    return retval
 
-    def generate_ngrams_range(self, s, ns, nf):
-        retval = []
-        for n in np.arange(ns, nf + 1):
-            if self.subword_mode == "word":
-                retval += self.generate_word_ngrams(s, n)
-            elif self.subword_mode == "char":
-                retval += self.generate_ngrams(s, n)
-        return retval
+
+@numba.jit(nopython=True, fastmath=True)
+def row_argmax(S_data, S_indices, S_indptr, Nr, Nc):
+    hits = np.zeros(Nr)
+    s = np.zeros(Nr)
+    for i in range(Nr):
+        w = S_data[S_indptr[i] : S_indptr[i + 1]]
+        if len(w) == 0:
+            continue
+        ind = np.argmax(w)
+        hits[i] = S_indices[S_indptr[i] + ind]
+        s[i] = w[ind]
+    return hits, s
